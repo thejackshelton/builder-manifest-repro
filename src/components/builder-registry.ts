d2bb533711eb829e2a0f6a3815c3eec30b4a2a4d
jack shelton <me@jackshelton.com>
2024-09-20 12:10:17 -0500

    any qwik library

--- src/components/builder-registry.ts
+++ src/components/builder-registry.ts
@@ -1,10 +1,24 @@
 import type { RegisteredComponent } from "@builder.io/sdk-qwik";
 import { Label } from "@qwik-ui/headless";
+import { LibLabel } from "my-qwik-library-name";
 
 export const CUSTOM_COMPONENTS: RegisteredComponent[] = [
   {
     component: Label,
-    name: "Label",
+    name: "Qwik UI Label",
+    defaultChildren: [
+      {
+        "@type": "@builder.io/sdk:Element",
+        component: {
+          name: "Label",
+          options: { text: "Label Text" },
+        },
+      },
+    ],
+  },
+  {
+    component: LibLabel,
+    name: "New Qwik Library Label",
     defaultChildren: [
       {
         "@type": "@builder.io/sdk:Element",
