854c2a8a346c1770505153c0f32ae6feb8c5581f
jack shelton <me@jackshelton.com>
2024-10-09 18:41:37 -0500

    builder manifest repro

--- src/components/builder-registry.ts
+++ src/components/builder-registry.ts
@@ -1,32 +1,3 @@
 import type { RegisteredComponent } from "@builder.io/sdk-qwik";
-import { Label } from "@qwik-ui/headless";
-import { LibLabel } from "my-qwik-library-name";
 
-export const CUSTOM_COMPONENTS: RegisteredComponent[] = [
-  {
-    component: Label,
-    name: "Qwik UI Label",
-    defaultChildren: [
-      {
-        "@type": "@builder.io/sdk:Element",
-        component: {
-          name: "Label",
-          options: { text: "Label Text" },
-        },
-      },
-    ],
-  },
-  {
-    component: LibLabel,
-    name: "New Qwik Library Label",
-    defaultChildren: [
-      {
-        "@type": "@builder.io/sdk:Element",
-        component: {
-          name: "Label",
-          options: { text: "Label Text" },
-        },
-      },
-    ],
-  },
-];
+export const CUSTOM_COMPONENTS: RegisteredComponent[] = [];
